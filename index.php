86ae79f0ac981c911366a882bd28bbf72d9f279c
Jordan Abujaber <jajaber13@gmail.com>
2015-05-07 09:01:16 -0700

    Video 3 done

--- index.php
+++ index.php
@@ -5,15 +5,27 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //Make Constants using define.
-define('client_id', '4503b95fad07470b9a05e553d7e666e0');
-define('client_secret', '2260286d766040f8a0ee05ed1cc2be9c');
+define('client_ID', '4503b95fad07470b9a05e553d7e666e0');
+define('client_Secret', '2260286d766040f8a0ee05ed1cc2be9c');
 define('redirectURI', 'http://localhost:8888/appacademyapi/index.php');
 define('ImageDirectory', 'pics/');
 ?>
 
-
-<!-- CLIENT INFO
-CLIENT ID	4503b95fad07470b9a05e553d7e666e0
-CLIENT SECRET	2260286d766040f8a0ee05ed1cc2be9c
-WEBSITE URL	http://localhost:8888/appacademyapi/index.php
-REDIRECT URI	http://localhost:8888/appacademyapi/index.php -->
+<!DOCTYPE html>
+<html>
+	<head>
+    	<meta charset="utf-8">
+    	<meta name="description" content="">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Untitled</title>
+		<link rel="stylesheet" type="text/style.css">
+		<link rel="author" href="humans.txt">
+</head>
+<body>
+	<!-- Creating a login for people to go and give approval for our web app to access their Instagram account 
+	After getting approval, we are now going to have the information so that we can play with it 
+	-->
+	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID; ?> &redirect_uri=<?php echo redirectURI?>&response_type=code">LOGIN</a>
+	<script src="js/main.js"></script>
+</body>
+</html>
