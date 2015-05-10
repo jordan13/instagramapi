12680aac6955a07f81847056b6b38aa276752778
Jordan Abujaber <jajaber13@gmail.com>
2015-05-10 16:04:27 -0700

    Video 8 done

--- index.php
+++ index.php
@@ -26,9 +26,16 @@ curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings); // setting the P
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); //setting it equal to 1 because we are geting strings back. 
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // but in live work production we want to set this to true.
 
-}
+
 $result = curl_exec($curl);
 curl_close();
+
+$results = json_decode($result, true);
+echo $results['user']['username'];
+}
+else {
+	
+}
 ?>
 
 <!DOCTYPE html>
